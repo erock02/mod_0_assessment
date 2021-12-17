97bc626e2e933fd51c5461e9a6518f85c5f1a08e
Eric Espindola Chavez <eric_e_2012@hotmail.com>
2021-12-17 17:07:04 -0600

    add developer model hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  first_name: "Eric",
+  last_name: "Espindola",
+  age: 27,
+  hands: 2,
+  uses_google: true,
+  is_funny: false,
+  previous_jobs: ["Expedia", "EOS"],
+  skills: ["SQL","VBA"]
+}

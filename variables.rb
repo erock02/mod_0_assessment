2393e016373a7f19b004ff837a01d7a480b7a1e6
Eric Espindola Chavez <eric_e_2012@hotmail.com>
2021-12-17 16:45:09 -0600

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,9 @@
+movie = "Life is Beautiful"
+is_entertaining = true
+movie = "Catch Me If You Can"
+num_4 = 12
+num_6 = 21
+product = num_4 * num_6
+favorite_foods = ["Pho", "Korean BBQ", "Enchiladas"]
+favorite_foods.shift
+movie.length

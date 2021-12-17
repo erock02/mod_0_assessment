c44546ac77e3815de4017e852c94b0a7d9a01641
Eric Espindola Chavez <eric_e_2012@hotmail.com>
2021-12-17 16:59:50 -0600

    add dinosaur model hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  dino_name: "T-rex",
+  diet: "carnivour",
+  age: 15,
+  height_in_inches: 144.25,
+  is_dangerous: true,
+  is_cool: true,
+  facts: ["Tyrannosaurus Rex means 'Tyrant Lizard'.", "The largest T. Rex tooth found is 12 inches (30 cm) long"],
+  fav_foods: ["other dinosaurs"]
+}
